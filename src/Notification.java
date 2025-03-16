812b1e2aecf6ac510189c7fc78bc4d537065d2d1
CarinoRB <rbc5967@students.uc-bcf.edu.ph>
2025-03-17 07:55:45 +0800

    Create Notification.java

---
+++ src/Notification.java
@@ -0,0 +1,14 @@
+public class Notification{
+    private int notificationId;
+    private String message;
+    private Date sentDate;
+    private NotificationType type;
+    
+    public void sendNotification(){
+        
+    }
+    
+    public int getNotificationId(){
+        return notificationId;
+    }
+}

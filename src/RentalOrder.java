b29e7258025373e4be1ea9e911580c495e4586f6
puyaojcs <jsp4995@students.uc-bcf.edu.ph>
2025-03-17 09:03:04 +0800

    Create RentalOrder.java

---
+++ src/RentalOrder.java
@@ -0,0 +1,24 @@
+class RentalOrder {
+        private int orderId;
+        private Date startDate;
+        private Date endDate;
+        private double cost;
+        private OrderStatus status;
+        public void createOrder(){
+
+        }
+        public void updateStatus(OrderStatus status){
+
+        }
+        public void cancelOrder(){
+
+        }
+        public void calculateCost(){
+
+        }
+        public int geOrderId(){
+          return orderId;
+        }
+        public double getCost(){
+          return cost;
+        }

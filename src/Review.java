9ad564cc992dccb9d16e47d8ffb813d210ea324e
VillareyesJMN <jnv0285@students.uc-bcf.edu.ph>
2025-03-17 08:24:12 +0800

    Create Review.java

---
+++ src/Review.java
@@ -0,0 +1,15 @@
+public class Review {
+  private int reviewID;
+  private int rating;
+  private String comments;
+  
+  public void submitReview(){
+    //submitReview logic
+  }
+  public void editReview(){
+    //editReview logic
+  }
+  public int getReviewId(){
+    return reviewId;
+  }
+}

57579bde1a1fd238fecf36e6f57b42eef34df090
VillareyesJMN <jnv0285@students.uc-bcf.edu.ph>
2025-03-17 08:29:32 +0800

    Create OrderStatus.java

---
+++ src/OrderStatus.java
@@ -0,0 +1,7 @@
+public enum OrderStatus {
+  PENDING, //Pending Status
+  CONFIRMED, //Confirmed Status
+  Active, //Active Status
+  COMPLETED, //Completed Status
+  CANCELLED, //Cancelled Status
+}

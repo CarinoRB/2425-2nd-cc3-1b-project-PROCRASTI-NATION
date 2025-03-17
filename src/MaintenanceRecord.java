de23b3304a92ba33915fd80877742a69bcc3a7f1
VillareyesJMN <jnv0285@students.uc-bcf.edu.ph>
2025-03-17 08:07:22 +0800

    Create MaintenanceRecord.java

---
+++ src/MaintenanceRecord.java
@@ -0,0 +1,13 @@
+public class MaintenanceRecord {
+  private int recordId;
+  private Date maintenanceDate;
+  private String description;
+  private Double cost;
+  
+  public void logMaintenance(){
+    //log maintenance logic
+  }
+  public int getRecordId(){
+    return recordId;
+  }
+}

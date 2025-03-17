3152081414f8ae2b6357807bf0fd995af22d7aa9
CarinoRB <rbc5967@students.uc-bcf.edu.ph>
2025-03-17 09:24:50 +0800

    Create Starting GUI using swing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.*;
+import javax.swing.*;
+
 public class Main{
     public static void main(String[]args){
-        System.out.println("Test");
+        JFrame frame = new JFrame();
+
+        // Creating instance of JButton
+        JButton button = new JButton("Team Procrasti-Nation");
+
+        // x axis, y axis, width, height
+        button.setBounds(150, 200, 220, 50);
+
+        // adding button in JFrame
+        frame.add(button);
+
+        // 400 width and 500 height
+        frame.setSize(500, 600);
+
+        // using no layout managers
+        frame.setLayout(null);
+
+        // making the frame visible
+        frame.setVisible(true);
     }
 }

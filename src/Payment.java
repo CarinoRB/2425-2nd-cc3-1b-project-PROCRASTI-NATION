4ca1b417d3767847085f8a964198cbed2d5842b0
puyaojcs <jsp4995@students.uc-bcf.edu.ph>
2025-03-17 08:37:18 +0800

    Create Payment.java

---
+++ src/Payment.java
@@ -0,0 +1,20 @@
+class Payment{
+
+  private int paymentId;
+  private double amount;
+  private String paymentDate;
+  private String status;
+
+  public void processPayment(){
+  //process payment
+  }
+  public void refundPayment(){
+  //payment refunding
+  }
+  public int getPaymendId(){
+    return paymentId;
+  }
+  public double getAmount(){
+    return amount;
+  }
+}

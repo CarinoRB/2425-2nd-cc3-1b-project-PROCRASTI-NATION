b90cb6389047b515d2f5a7d1e44a756e2b218e4c
VillareyesJMN <jnv0285@students.uc-bcf.edu.ph>
2025-03-17 08:17:28 +0800

    Create Insurance.java

---
+++ src/Insurance.java
@@ -0,0 +1,14 @@
+public class Insurance {
+  private int insuranceID;
+  private String provider;
+  private String policyNUmber;
+  private double coverageAmount;
+  private double dailyPremium;
+  
+  public void calculatePremium(){
+    //calculatePremium logic 
+  }
+  public int getinsuranceId(){
+    return insuranceId;
+  }
+}

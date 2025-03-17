43e908056a7f362232a2eff4cb10b0f115d13f93
VillareyesJMN <jnv0285@students.uc-bcf.edu.ph>
2025-03-17 08:32:30 +0800

    Update OrderStatus.java

--- src/OrderStatus.java
+++ src/OrderStatus.java
@@ -1,7 +1,7 @@
 public enum OrderStatus {
   PENDING, //Pending Status
   CONFIRMED, //Confirmed Status
-  Active, //Active Status
+  ACTIVE, //Active Status
   COMPLETED, //Completed Status
   CANCELLED, //Cancelled Status
 }
